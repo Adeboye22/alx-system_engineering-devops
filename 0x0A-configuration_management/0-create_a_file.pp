d49a278e4c8c795acc30152d3d59ef9827a3d584
Oluwadamilola <OyeniyaOluwadamilola@outlook.com>
2022-09-16 14:55:53 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create a file
-file { '/tmp/holberton':
-  ensure  => file,
-  path    => '/tmp/holberton',
+
+file { 'school':
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  path    => '/tmp/school',
 }
